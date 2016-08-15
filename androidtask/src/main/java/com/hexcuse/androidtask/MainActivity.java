987170d0a025a9efbcc28b362b25d6a560b4b724
duanfazhu <bird1080@163.com>
2016-08-15 18:07:57 +0800

    Service BroadCast

--- androidtask/src/main/java/com/hexcuse/androidtask/MainActivity.java
+++ androidtask/src/main/java/com/hexcuse/androidtask/MainActivity.java
@@ -1,7 +1,9 @@
 package com.hexcuse.androidtask;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,5 +13,14 @@ public class MainActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		((TextView)findViewById(R.id.tv)).setText(getClass().getSimpleName());
+
+		findViewById(R.id.btn).setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Intent intent = new Intent(getApplicationContext(), TwoActivity.class);
+				intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+				startActivity(intent);
+			}
+		});
 	}
 }
