742b32d838d8211b0e854bf55cd5a63abbf1b0f5
duanfazhu <bird1080@163.com>
2016-08-15 18:09:52 +0800

    Service BroadCast

---
+++ androidtask/src/main/java/com/hexcuse/androidtask/ThreeActivity.java
@@ -0,0 +1,27 @@
+package com.hexcuse.androidtask;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+/**
+ * Created by Hexcuse on 2016/8/15.
+ *
+ * @Package com.hexcuse.androidtask.
+ * @ClassName: ${CLASS_NAME}.
+ * @Description: ${todo}(用一句话描述该文件做什么).
+ */
+public class ThreeActivity extends Activity {
+
+	private final String TAG = getClass().getSimpleName();
+	private final int TASK = getTaskId();
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		((TextView)findViewById(R.id.tv)).setText(getClass().getSimpleName());
+		Log.d(TAG, String.valueOf(TASK));
+	}
+}
